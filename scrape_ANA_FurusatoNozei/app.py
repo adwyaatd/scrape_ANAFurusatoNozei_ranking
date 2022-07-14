2732adee5dd71d8c92af752d1d25a23871ad9e18
細田 来夢 <hosoda_raimu@cyberagent.co.jp>
2022-07-14 22:50:01 +0900

    動作に必要な最小の処理を実装

--- scrape_ANA_FurusatoNozei/app.py
+++ scrape_ANA_FurusatoNozei/app.py
@@ -1,12 +1,18 @@
+from calendar import weekday
+import imp
 import json
 # from operator import neg
 import sys
 import os
+# from tkinter.tix import Tree
+# from tkinter import W
 import traceback
 import re
 # from turtle import end_fill
 import uuid
-from datetime import datetime
+import datetime
+import os
+from wsgiref.util import shift_path_info
 
 from selenium import webdriver
 from selenium.webdriver.chrome import options
@@ -15,6 +21,11 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.select import Select
 import boto3
 import pytz
+import gspread
+from gspread_formatting import *
+from oauth2client.service_account import ServiceAccountCredentials
+from dotenv import load_dotenv
+load_dotenv()
 
 
 def get_driver():
@@ -156,13 +167,194 @@ def get_current_datetime():
     return current_datetime
 
 
+def get_inclusive_index(list, purpose):
+    for index, element in enumerate(list):
+        if purpose in element: return index
 
-def write_spreadsheet(gift_list):
-    print(gift_list)
-    session = get_session
+    raise IndexError
 
-def get_session():
-    print('セッション開始')
+
+def get_parameters_from_SSM(parameter_name_list):
+    ssm = boto3.client('ssm')
+    parameters = dict()
+
+    ssm_response = ssm.get_parameters(
+        Names=parameter_name_list,
+        WithDecryption=True,
+    )
+
+    for param in ssm_response['Parameters']:
+        parameters[ param["Name"] ] = param["Value"]
+
+    return parameters
+
+
+
+def write_spreadsheet(scraped_gift_list):
+    sheet_name = "元総合"
+    sheet = get_Gspreed_sheet(sheet_name)
+
+    print('設定開始')
+    last_row_num = get_last_row_num(sheet)
+    new_row_num = last_row_num + 1
+    new_col_num = get_last_col_num(sheet) + 1
+
+    todays_date = datetime.date.today()
+    now_time = datetime.datetime.now().time().strftime('%X')
+    todays_weekday = get_weekday(todays_date)
+
+    print(f'new_col_num: {new_col_num}')
+    print(f'new_row_num: {new_row_num}')
+
+    sheet.update_cell(4,new_col_num,now_time)
+    sheet.update_cell(5,new_col_num,str(todays_date))
+    sheet.update_cell(6,new_col_num,todays_weekday)
+
+    print("本日の列の枠線設置開始")
+
+    # 範囲
+    todays_top_cel = gspread.utils.rowcol_to_a1(5, new_col_num)
+    todays_current_bottom_cel = gspread.utils.rowcol_to_a1(last_row_num, new_col_num)
+    todays_col_range = f'{todays_top_cel}:{todays_current_bottom_cel}'
+    todays_top_range = todays_top_cel
+
+    dot = Border("DOTTED", Color(0, 0, 0, 0))
+    solid = Border("SOLID", Color(0, 0, 0, 0))
+
+    fmt_td_bd_rs_ls_border = CellFormat(borders=Borders(top=dot,bottom=dot,left=solid,right=solid))
+    fmt_ts = CellFormat(borders=Borders(top=solid))
+    format_cell_range(sheet, todays_col_range, fmt_td_bd_rs_ls_border)
+    format_cell_range(sheet, todays_top_range, fmt_ts)
+
+    print('記録開始')
+
+    add_row_count = 0
+
+    last_row_num = get_last_row_num(sheet)
+    all_add_list = []
+    ranking_list = []
+    new_gift_No = int(sheet.cell(last_row_num,1).value)+1
+
+    # 取得済みの自治体名、返礼品名、金額をリストで取得
+    existing_gitf_area_list = sheet.col_values(2)
+    existing_gitf_name_list = sheet.col_values(3)
+    existing_gitf_price_list = sheet.col_values(4)
+    for scraped_gift in scraped_gift_list:
+        is_matched = False
+        transrate_table = str.maketrans('','',',円')
+        scraped_gift_price = int(scraped_gift['gift_price'].translate(transrate_table))
+        scraped_gift_area = scraped_gift['gift_area'].replace(' ','')
+        if scraped_gift['gift_name'] in existing_gitf_name_list:
+            print('返礼品名あり')
+            index = get_inclusive_index(existing_gitf_name_list, scraped_gift['gift_name'])
+            existing_gift_price = int(existing_gitf_price_list[index].translate(transrate_table))
+            existing_gift_area = existing_gitf_area_list[index].replace(' ','')
+            print(f'index: {index}')
+            print(f'scraped_gift_area: {scraped_gift_area}')
+            print(f'existing_gift_area: {existing_gift_area}')
+            print(f'scraped_gift_price: {scraped_gift_price}')
+            print(f'existing_gift_price: {existing_gift_price}')
+
+            if scraped_gift_area == existing_gift_area and scraped_gift_price == existing_gift_price:
+                print('全一致 返礼品あり')
+                is_matched = True
+                row_num = index+1
+                sheet.update_cell(row_num,new_col_num,scraped_gift["ranking"])
+                continue
+
+        if not is_matched:
+            print("合致なし。新規返礼品として登録")
+            add_list = [new_gift_No,scraped_gift_area,scraped_gift['gift_name'],scraped_gift_price]
+            all_add_list.append(add_list)
+            ranking_list.append([scraped_gift["ranking"]])
+            new_gift_No += 1
+            add_row_count += 1
+
+    if add_row_count >= 1:
+        print('新規返礼品あり。返礼品情報書き込み')
+        new_top_left_cel = gspread.utils.rowcol_to_a1(new_row_num, 1)
+        sheet.append_rows(all_add_list, table_range=new_top_left_cel)
+
+        print('ランキング書き込み')
+        new_top_right_cel = gspread.utils.rowcol_to_a1(new_row_num, new_col_num)
+        sheet.append_rows(ranking_list, table_range=new_top_right_cel)
+
+        print('枠線設置')
+        new_bottom_row_num = last_row_num + add_row_count
+        new_bottom_right_cel = gspread.utils.rowcol_to_a1(new_bottom_row_num, new_col_num)
+        add_range = f'{new_top_left_cel}:{new_bottom_right_cel}'
+        format_cell_range(sheet, add_range, fmt_td_bd_rs_ls_border)
+    else:
+        print('全て既存品のみで追加はなし')
+
+    print("スプレッドシートへの書き込み完了")
+    return True
+
+def get_Gspreed_sheet(sheet_name):
+    print('認証開始')
+
+    gsp_key_name_list = [
+        'gsp_ANA_FURUSATONOZEI_client_id',
+        'gsp_ANA_FURUSATONOZEI_project_id',
+        'gsp_ANA_FURUSATONOZEI_client_email',
+        'gsp_ANA_FURUSATONOZEI_client_x509_cert_url',
+        'gsp_ANA_FURUSATONOZEI_private_key',
+        'gsp_ANA_FURUSATONOZEI_private_key_id'
+    ]
+
+    gsp_keys_dict = get_parameters_from_SSM(gsp_key_name_list) 
+
+    project_id           = gsp_keys_dict['gsp_ANA_FURUSATONOZEI_project_id']
+    private_key_id       = gsp_keys_dict['gsp_ANA_FURUSATONOZEI_private_key_id']
+    private_key          = gsp_keys_dict['gsp_ANA_FURUSATONOZEI_private_key'].replace('\\n','\n')
+    client_email         = gsp_keys_dict['gsp_ANA_FURUSATONOZEI_client_email']
+    client_id            = gsp_keys_dict['gsp_ANA_FURUSATONOZEI_client_id']
+    client_x509_cert_url = gsp_keys_dict['gsp_ANA_FURUSATONOZEI_client_x509_cert_url']
+    
+    key_dict = {
+        "type": "service_account",
+        "project_id": project_id,
+        "private_key_id": private_key_id,
+        "private_key": private_key,
+        "client_email": client_email,
+        "client_id": client_id,
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": client_x509_cert_url
+    }
+
+    scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+
+    #ダウンロードしたjsonファイル名をクレデンシャル変数に設定。
+    credentials = ServiceAccountCredentials.from_json_keyfile_dict(key_dict, scope)
+    #OAuth2の資格情報を使用してGoogle APIにログイン。
+    session = gspread.authorize(credentials)
+
+    #スプレッドシートIDを変数に格納する。
+    key = '1Ww3kvLvsE9fEtcHIKfPl5hsTycvKEc2uRjM_oNxWtbI'
+    # スプレッドシート（ブック）を開く
+    sheets = session.open_by_key(key)
+
+    sheet = sheets.worksheet(sheet_name)
+    return sheet
+
+
+def get_last_col_num(sheet):
+    str_list = list(filter(None, sheet.row_values(5)))
+    return len(str_list)
+
+
+def get_last_row_num(sheet):
+    str_list = list(filter(None, sheet.col_values(1)))
+    return len(str_list)
+
+
+def get_weekday(date):
+    days = ['月', '火', '水', '木', '金', '土', '日']
+    weekday_num = date.weekday()
+    weekday = days[weekday_num]
+    return weekday
 
 
 def main(event):
@@ -170,19 +362,18 @@ def main(event):
     should_scrape = body["should_scrape"]
     is_success = False
     is_written = False
-    params_gift_list = body["gift_list"]
+    params_gift_list = body["demo_gift_list"]
 
-    gift_list = (scrape_ranking()) if should_scrape else params_gift_list
+    scraped_gift_list = (scrape_ranking()) if should_scrape else params_gift_list
 
     print("-------------------------")
-    print(f"gift_list: {gift_list}")
+    print(f"scraped_gift_list: {scraped_gift_list}")
     print("-------------------------")
 
-    if gift_list:
-        # is_written = write_spreadsheet(gift_list)
-        is_written = True
+    if scraped_gift_list:
+        is_written = write_spreadsheet(scraped_gift_list)
     else:
-        print("no gift_list")
+        print("no scraped_gift_list")
         is_success = False 
 
     if is_written:
@@ -218,10 +409,11 @@ def lambda_handler(event, context):
         print(value_)
         print(traceback_)
         print(traceback.format_exc())
+        print(e)
         print("----------")
         return {
             "statusCode": 400,
             "body": json.dumps({
-                "message": "Error!",
+                "message": e,
             })
         }
